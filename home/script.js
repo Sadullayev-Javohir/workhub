bf9f673127cbf9837d86be30f5b2aadc5968e711
Javohir Sadullayev <javohirsadullayev2024@gmail.com>
2024-12-29 16:26:08 +0500

    WorkHub

---
+++ home/script.js
@@ -0,0 +1,10 @@
+const carousel = new bootstrap.Carousel("#carouselExampleDark", {
+    interval: 2000,
+    ride: "carousel",
+});
+
+const carousel2 = new bootstrap.Carousel("#carouselExampleSlidesOnly", {
+    interval: 2000,
+    ride: "carousel",
+});
+
